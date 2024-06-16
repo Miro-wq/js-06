b4809ea63154c096d5131d18ccdc8b7808593574
Miro <Miro-wq@users.noreply.github.com>
2024-06-16 22:24:20 +0300

    update all

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const showColor = document.querySelector(".color");
+const btn = document.querySelector(".change-color");
+// document.querySelector(".btn").addEventListener("click", function () {
+//   document.body.style.backgroundColor = getRandomHexColor();
+// });
+
+btn.addEventListener("click", function onClick() {
+  const body = document.querySelector("body");
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  showColor.textContent = randomColor;
+});
--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,19 @@
+// IN CONSTRUCTIE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// NU ESTE GATA !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+const controls = document.getElementById("controls");
+const btnCreate = document.querySelector('button[data-create="Create"]');
+const btnDestroy = document.querySelector('button[data-destory="Destroy"]');
+const multyBoxes = document.getElementById("boxes");
+
+function createBoxes(amount) {
+  let result = multyBoxes + amount;
+  return result;
+}
+console.log(createBoxes(10));
